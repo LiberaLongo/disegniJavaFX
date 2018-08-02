7a7a4d7216fdffc1ba6a31ff9e37a7452495d0fd
liberalongo@gmail.com <liberalongo@gmail.com>
2018-08-02 17:18:39 +0200

    inizio rotazioni

--- trasportations/MCskin.java
+++ trasportations/MCskin.java
@@ -1,8 +1,9 @@
-package ksisters;
+package trasportations;
 
 import javafx.scene.Group;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.transform.Rotate;
 
 public class MCskin {
 	private Double x;
@@ -10,6 +11,7 @@ public class MCskin {
 	private Double size;
 	private Color colori[];
 	private Integer matrix[][];
+	private Rotate rotate;
 
 	public MCskin(Double x, Double y, Double size, Color[] colori, Integer[][] matrix) {
 		super();
@@ -18,6 +20,17 @@ public class MCskin {
 		this.size = size;
 		this.colori = colori;
 		this.matrix = matrix;
+		this.rotate = new Rotate();
+	}
+
+	public MCskin(Double x, Double y, Double size, Color[] colori, Integer[][] matrix, Rotate rotate) {
+                super();
+                this.x = x;
+                this.y = y;
+                this.size = size;
+                this.colori = colori;
+                this.matrix = matrix;
+                this.rotate = rotate;
 	}
 
 	public Group draw(Group root) {
@@ -27,6 +40,8 @@ public class MCskin {
                                 Rectangle pixel = new Rectangle(this.x + j * this.size, this.y + i*this.size, this.size, this.size);
                                 //coloro il pixel tramite l'indice della matrice che mi indica la posizione del colore
 				pixel.setFill(this.colori[this.matrix[i][j]]);
+                                //il comando sotto fa eventuali rotazioni
+				pixel.getTransforms().addAll(this.rotate);
 				//aggiungo il pixel al resto del Group
 				root.getChildren().add(pixel);
                         }
