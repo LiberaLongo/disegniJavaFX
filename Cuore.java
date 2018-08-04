607729ee83284b0197bc3377cf19cdcef4e66470
liberalongo@gmail.com <liberalongo@gmail.com>
2018-08-04 12:15:55 +0200

    aggiunti cuoricini

---
+++ Cuore.java
@@ -0,0 +1,51 @@
+package ksisters;
+
+import javafx.scene.Group;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Shape;
+
+public class Cuore {
+	private Double x;
+	private Double y;
+	private Double size;
+	private Color paint;
+
+	Cuore() {
+	}
+	Cuore(Double x, Double y, Double size) {
+		this.x = x;
+		this.y = y;
+		this.size = size;
+		this.paint = Color.RED;
+	}
+	Cuore(Double x, Double y, Double size, Color paint) {
+		this.x = x;
+		this.y = y;
+		this.size = size;
+		this.paint = paint;
+	}
+	public Shape draw() {
+		Circle sinistro = new Circle(this.x-this.size*4/5, this.y, this.size);
+		Circle destro = new Circle(this.x+this.size*4/5, this.y, this.size);
+		Shape cerchi = Shape.union(sinistro, destro);		
+		Polygon sotto = new Polygon();
+		sotto.getPoints().addAll(new Double[]{
+			this.x-this.size*3/2+1, this.y+this.size*3/4,
+			this.x, this.y,
+			this.x+this.size*3/2-1, this.y+this.size*3/4,
+			this.x, this.y+this.size*2
+		});
+		Shape cuore = Shape.union(cerchi, sotto);
+		cuore.setFill(this.paint);
+		return cuore;
+	}
+	public Shape draw(Double x, Double y, Double size, Color paint) {
+		this.x = x;
+		this.y = y;
+		this.size = size;
+		this.paint = paint;
+		return this.draw();
+	}
+}
---
+++ KsistersCuori.java
@@ -0,0 +1,143 @@
+package ksisters;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Shape;
+
+public class KsistersCuori extends Application {
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+                //variabili spaziali
+                Double size = 40.;      	//dimensione di un pixel
+		Double sizeCuori = size;	//dimensione del raggio del cerchio di un cuoricino
+		Double space = sizeCuori*2;	//spazietto
+                Double spazioX = size*8+space;		//spazio lungo l'orizzontale ---
+		Double spazioY = size*4+space/5;	//spazio lungo la verticale |
+                Double width = (size*4)*2+spazioX*2+space*2;
+		Double height = (size*4)*2+spazioY*2+space/2;
+		//KIRIAIR
+		Color coloriKIRIAIR[] = {
+			Color.PINK,
+			Color.MAROON,
+			Color.BLACK,
+			Color.WHITE,
+			Color.GREEN,
+			Color.RED,
+			Color.FUCHSIA
+		};
+		Integer matrixKIRIAIR[][] = {
+			{1, 6, 1, 1, 1, 1, 1, 1},
+			{6, 1, 1, 0, 0, 1, 1, 1},
+			{1, 2, 2, 0, 0, 2, 2, 1},
+			{2, 4, 3, 0, 0, 3, 4, 2},
+			{0, 4, 2, 0, 0, 2, 4, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0, 0},
+			{1, 0, 0, 5, 5, 0, 0, 1},
+		};
+		//KOMAKI
+		Color coloriKoMaKi_[] = {
+			Color.PINK,
+			Color.DARKORANGE,
+			Color.BLACK,
+			Color.WHITE,
+			Color.DARKVIOLET,
+			Color.PURPLE,
+			Color.LIGHTGREY,
+		};
+		Integer matrixKoMaKi_[][] = {
+			{6, 2, 2, 1, 1, 2, 2, 6},
+			{2, 4, 4, 2, 2, 4, 4, 2},
+			{2, 4, 5, 2, 2, 4, 5, 2},
+			{1, 2, 2, 1, 2, 2, 2, 1},
+			{1, 1, 1, 2, 0, 2, 2, 1},
+			{1, 1, 2, 0, 0, 4, 3, 2},
+			{1, 2, 5, 0, 0, 5, 3, 1},
+			{2, 2, 0, 0, 0, 0, 0, 1},
+		};
+		//KiraLushia
+		Color coloriKiraLushia[] = {
+			Color.PINK,	//pelle
+			Color.HOTPINK,	//capelli
+			Color.BLACK,
+			Color.WHITE,
+			Color.RED, 	//occhio <-
+			Color.ORCHID,
+			Color.BLUE, //occhio    ->
+			Color.AQUA
+		};
+		Integer matrixKiraLushia[][] = {
+			{1, 1, 1, 1, 1, 1, 1, 1},
+			{1, 1, 1, 1, 1, 1, 1, 1},
+			{1, 1, 1, 1, 1, 1, 1, 1},
+			{1, 2, 2, 1, 1, 2, 2, 1},
+			{2, 3, 5, 0, 0, 7, 3, 2},
+			{1, 3, 4, 0, 0, 6, 3, 1},
+			{1, 0, 0, 0, 0, 0, 0, 1},
+			{1, 0, 0, 0, 0, 0, 0, 1},
+		};
+		//LLibera (the autor)
+		Color coloriLLibera[] = {
+			Color.PINK,
+			Color.RED,
+			Color.ORANGE,
+			Color.YELLOW,
+			Color.GREEN,
+			Color.AQUA,
+			Color.BLUE,
+			Color.PURPLE,
+			Color.BLACK,
+			Color.WHITE,
+			Color.FUCHSIA,
+			Color.SANDYBROWN
+		};
+		Integer matrixLLibera[][] = {
+			{ 5, 6, 7, 1, 2, 3, 4, 5},
+			{ 6, 7, 1, 2, 3, 4, 5, 6},
+			{ 7, 8, 8, 0, 0, 8, 8, 7},
+			{ 8,10, 9, 0, 0, 9, 10, 8},
+			{ 0,10, 8, 0, 0, 8, 10, 0},
+			{ 0, 0, 0, 11, 11, 0, 0, 0},
+			{ 0, 0, 1, 0, 0, 1, 0, 0},
+			{11, 0, 0, 1, 1, 0, 0, 11},
+		};
+		//centro della finestra
+		Double x = width/2;
+		Double y = height/2;
+		//Disegno dei soggetti
+		Group root = new Group();
+		//dei cuori animati e pucciosissimi
+		Cuore cuore1 = new Cuore(x - spazioX, y - spazioY-space*5/4, sizeCuori);	//in alto a sinistra
+		Cuore cuore2 = new Cuore(x + spazioX, y - spazioY-space*5/4, sizeCuori);	//in alto a destra
+		Cuore cuore3 = new Cuore(x - spazioX, y + spazioY+space*4/5, sizeCuori);	//in basso a sinistra
+		Cuore cuore4 = new Cuore(x + spazioX, y + spazioY+space*4/5, sizeCuori);	//in basso a destra
+		Shape cuoreS1 = cuore1.draw();
+		Shape cuoreS2 = cuore2.draw();
+		Shape cuoreS3 = cuore3.draw();
+		Shape cuoreS4 = cuore4.draw();
+		root.getChildren().addAll(cuoreS1, cuoreS2, cuoreS3, cuoreS4);
+		//le Ksisters
+		MCskin KIRIAIR 		= new MCskin(x, y+spazioY, size, coloriKIRIAIR, matrixKIRIAIR);
+		MCskin KoMaKi_ 		= new MCskin(x+spazioX, y, size, coloriKoMaKi_, matrixKoMaKi_);
+		MCskin KiraLushia 	= new MCskin(x-spazioX, y, size, coloriKiraLushia, matrixKiraLushia);
+		MCskin LLibera 		= new MCskin(x, y-spazioY, size, coloriLLibera, matrixLLibera);
+		root = LLibera.draw(root);
+		root = KoMaKi_.draw(root);
+		root = KiraLushia.draw(root);
+		root = KIRIAIR.draw(root);
+		//Stampa tutto
+		Scene scene = new Scene(root , width, height);
+		primaryStage.setTitle("Ksister fan-code by LLibera");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	public static void main(String args[]){
+		launch(args);
+	}
+}
+
