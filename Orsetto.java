d19ac2ccdedb064dcc9b2a99522749fc9e4e2306
liberalongo@gmail.com <liberalongo@gmail.com>
2018-08-08 20:22:23 +0200

    corpo

--- Orsetto.java
+++ Orsetto.java
@@ -10,15 +10,19 @@ import javafx.scene.paint.Color;
 public class Orsetto extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		//colori
+		Color pellechiara = Color.SANDYBROWN;
+		Color pelle = Color.BROWN;
 		//variabili ampiezza schermo
 		Double widthScene = 1000.0, heightScene = 700.0;
 		//variabili coordinate del soggetto
 		Double x = widthScene*1/2, y = heightScene*1/2;
 		//proporzioni fisiche del soggetto
-		//testa
+
+		//TESTA
 		Double Xtesta = x;
-		Double Ytesta = y;
-		Double raggioTesta = 200.0;
+		Double Ytesta = y-200;
+		Double raggioTesta = 100.0;
 		//orecchie esterne
 		Double distanzaOrecchie = raggioTesta*2/3;
 		Double altezzaOrecchie = raggioTesta*2/3;
@@ -48,32 +52,47 @@ public class Orsetto extends Application {
 		Double radiusXNaso = raggioTesta*1/8;
 		Double radiusYNaso = raggioTesta*1/10;
 
+		//CORPO
+		Double radiusXCorpo = raggioTesta*2/3;
+		Double radiusYCorpo = raggioTesta*2;
+		//pancia
+		Double radiusXPancia = radiusXCorpo*2/3;
+		Double radiusYPancia = radiusYCorpo*2/3;
+		//braccia
+		Double attaccaturaAltezzaBraccia = raggioTesta;
+		Double attaccaturaDistanzaBraccia = raggioTesta;
+		Double angoloBraccia = 20.;
+		//gambe
+		Double attaccaturaAltezzaGambe = raggioTesta;
+		Double attaccaturaDistanzaGambe = raggioTesta;
+		Double angoloGambe = 60.;
+
 		//Disegno del soggetto
+		//TESTA
 		//cranio
 		Circle cranio = new Circle(Xtesta, Ytesta, raggioTesta);
-		//shape per fare orecchie interne
-		Circle tD = cranio;
-		Circle tS = cranio;
 		//orecchie esterne
 		Circle orecchioSinistro = new Circle(Xtesta-distanzaOrecchie, Ytesta-altezzaOrecchie, raggioOrecchie);
 		Circle orecchioDestro = new Circle(Xtesta+distanzaOrecchie, Ytesta-altezzaOrecchie, raggioOrecchie);
 		Shape orecchie = Shape.union(orecchioSinistro, orecchioDestro);
 		//testa
 		Shape testa = Shape.union(cranio, orecchie);
-		testa.setFill(Color.BLACK);
+		testa.setFill(pelle);
 		//orecchie interne
 		//preparazione orecchie interne
+                Circle tD = cranio;
+                Circle tS = cranio;
 		Circle internosinistro = new Circle(Xtesta-distanzaOrecchieInterne, Ytesta-altezzaOrecchieInterne, raggioOrecchieInterne);
 		Circle internodestro = new Circle(Xtesta+distanzaOrecchieInterne, Ytesta-altezzaOrecchieInterne, raggioOrecchieInterne);
 		Shape orecchioInternoSinistro = Shape.subtract(internosinistro, tS);
 		Shape orecchioInternoDestro = Shape.subtract(internodestro, tD);
 		Shape orecchieInterne = Shape.union(orecchioInternoSinistro, orecchioInternoDestro);
-		orecchieInterne.setFill(Color.SANDYBROWN);
+		orecchieInterne.setFill(pellechiara);
 		//orbite
 		Ellipse orbitaSinistra = new Ellipse(Xtesta-distanzaOrbite, Ytesta, radiusXOrbite, radiusYOrbite);
 		Ellipse orbitaDestra = new Ellipse(Xtesta+distanzaOrbite, Ytesta, radiusXOrbite, radiusYOrbite);
 		Shape orbite = Shape.union(orbitaSinistra, orbitaDestra);
-		orbite.setFill(Color.SANDYBROWN);
+		orbite.setFill(pellechiara);
 		//occhi
 		Ellipse occhioSinistro = new Ellipse(Xtesta-distanzaOcchi, Ytesta, radiusXOcchi, radiusYOcchi);
 		Ellipse occhioDestro = new Ellipse(Xtesta+distanzaOcchi, Ytesta, radiusXOcchi, radiusYOcchi);
@@ -87,14 +106,20 @@ public class Orsetto extends Application {
 		pupille.setStrokeWidth(2);
 		//bocca
 		Ellipse bocca = new Ellipse(Xtesta, Ytesta+altezzaBocca, radiusXBocca, radiusYBocca);
-		bocca.setFill(Color.SANDYBROWN);
+		bocca.setFill(pellechiara);
 		//naso
 		Ellipse naso = new Ellipse(Xtesta, Ytesta+altezzaNaso, radiusXNaso, radiusYNaso);
-		naso.setFill(Color.BLACK);
+		naso.setFill(pelle);
+
+		//CORPO
+		Ellipse corpo = new Ellipse(x, y, radiusXCorpo, radiusYCorpo);
+		corpo.setFill(pelle);
+		Ellipse pancia = new Ellipse(x, y, radiusXPancia, radiusYPancia);
+		pancia.setFill(pellechiara);
 
 		//stampa tutto
 		Group topolino = new Group();
-		topolino.getChildren().addAll(testa, orbite, occhi, pupille, bocca, naso, orecchieInterne);
+		topolino.getChildren().addAll(corpo, pancia, testa, orbite, occhi, pupille, bocca, naso, orecchieInterne);
 		Scene scene = new Scene(topolino ,widthScene, heightScene);
 		primaryStage.setTitle("Orsetto");
 		primaryStage.setScene(scene);
