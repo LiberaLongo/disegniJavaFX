bf1ffc3fff92619fa3623e39e441363f9f159c43
liberalongo@gmail.com <liberalongo@gmail.com>
2018-08-31 17:51:07 +0200

    anche ubuntu interno monocolore, comunque non riesco a capire perchè non posso metterlo come shape simbol a cui sottraggo le varie parti man mano

--- ubuntuSimbol/disegnoUbuntu.java
+++ ubuntuSimbol/disegnoUbuntu.java
@@ -5,6 +5,7 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.shape.Shape;
+import javafx.scene.paint.Color;
 
 public class disegnoUbuntu extends Application {
 	@Override
@@ -12,11 +13,16 @@ public class disegnoUbuntu extends Application {
 		//variabili ampiezza schermo
 		Double width = 1300.0, height = 700.0;
 		Double x = width/2, y = height/2;
+		Double distance = width/4;
 		//cosa voglio disegnare
-		simbolUbuntu ubuntu = new simbolUbuntu (x, y, 250.);
+		simbolUbuntu ubuntuRAG = new simbolUbuntu (x - distance, y, 100.);
+		simbolUbuntu ubuntuRGB = new simbolUbuntu (x + distance, y, 100., Color.RED, Color.YELLOW, Color.BLUE);
+		simbolUbuntu monocolor = new simbolUbuntu (x, y, 100., Color.PURPLE);
 		//Stampa tutto
 		Group root = new Group();
-		root = ubuntu.draw(root);
+		root = ubuntuRAG.draw(root);
+		root = ubuntuRGB.draw(root);
+		root = monocolor.draw(root);
 		Scene scene = new Scene(root , width, height);
 		primaryStage.setTitle("Simbol of Ubuntu");
 		primaryStage.setScene(scene);
--- ubuntuSimbol/simbolUbuntu.java
+++ ubuntuSimbol/simbolUbuntu.java
@@ -11,6 +11,7 @@ public class simbolUbuntu {
 	private Double x;
 	private Double y;
 	private Double size;
+	private Color colorEsterno;
 	private Color colore1;
 	private Color colore2;
 	private Color colore3;
@@ -25,6 +26,13 @@ public class simbolUbuntu {
 		this.colore3 = Color.YELLOW;
 		this.interno = true;
 	}
+	simbolUbuntu(Double x, Double y, Double size, Color esterno) {
+		this.x = x;
+		this.y = y;
+		this.size = size;
+		this.colorEsterno = esterno;
+		this.interno = false;
+	}
 	simbolUbuntu(Double x, Double y, Double size, Color colore1, Color colore2, Color colore3) {
 		this.x = x;
 		this.y = y;
@@ -34,35 +42,26 @@ public class simbolUbuntu {
 		this.colore3 = colore3;
 		this.interno = true;
 	}
-	simbolUbuntu(Double x, Double y, Double size, Color colore1, Color colore2, Color colore3, Boolean interno) {
-		this.x = x;
-		this.y = y;
-		this.size = size;
-		this.colore1 = colore1;
-		this.colore2 = colore2;
-		this.colore3 = colore3;
-		this.interno = interno;
-	}
 
 	public Group draw(Group root) {
 		//Proporzioni
 		Double proporzione = this.size/3;
 		Double proporzioneSpazi = proporzione/4;
 		//raggi
-		Double raggioEsterno = this.size;
+		Double raggioEsterno = this.size*3/4;
 		Double raggioInterno = raggioEsterno - proporzione;
 		Double raggioTeste = proporzione/2;	/* il simbolo di Ubuntu simboleggia fratellanza
 							*  sono 3 persone di colori diversi che si danno la mano
 							*  in modo molto schematico */
 		Double raggioSpazio = raggioTeste + proporzioneSpazi;
 		//
-		Double distanza = this.size;
+		Double distanza = raggioEsterno;
 		//cerchi di preparazione
 		//Nota: testa1 è a pigreco/3, testa2 a pigreco, testa3 a 5/3*pigreco
 		//preparazione delle braccia
-		Circle esterno = new Circle (x, y, raggioEsterno);
+		Circle medio = new Circle (x, y, raggioEsterno);	//esterno se this.interno=true;
 		Circle interno = new Circle (x, y, raggioInterno);
-		Shape bracciaUnite = Shape.subtract (esterno, interno);		
+		Shape bracciaUnite = Shape.subtract (medio, interno);		
 		Circle spazioTesta1 = new Circle (x + distanza*Math.cos(Math.PI*1/3), y - distanza*Math.sin(Math.PI*1/3), raggioSpazio);
 		bracciaUnite = Shape.subtract(bracciaUnite, spazioTesta1);
 		Circle spazioTesta2 = new Circle (x + distanza*Math.cos(Math.PI*3/3), y - distanza*Math.sin(Math.PI*3/3), raggioSpazio);
@@ -94,7 +93,29 @@ public class simbolUbuntu {
 		testa2.setFill(this.colore2);
 		Circle testa3 = new Circle (x + distanza*Math.cos(Math.PI*5/3), y - distanza*Math.sin(Math.PI*5/3), raggioTeste);
 		testa3.setFill(this.colore3);
-		root.getChildren().addAll(braccia1, braccia2, braccia3, testa1, testa2, testa3);
+		if(!this.interno ) {			
+			Circle esterno = new Circle (x, y, size);
+			esterno.setFill(this.colorEsterno);
+			//codice che non sembra funzionare ma non ne capisco il motivo
+			//sembra che le braccia e le teste non siano interne al cerchio "esterno"
+			//o che ci siano altri problemi
+			/*Shape simbol = Shape.subtract(esterno, braccia1);
+			simbol = Shape.subtract(simbol, braccia2);
+			simbol = Shape.subtract(simbol, braccia3);
+			simbol = Shape.subtract(simbol, testa1);
+			simbol = Shape.subtract(simbol, testa2);
+			simbol = Shape.subtract(simbol, testa3);
+			simbol.setFill();
+			root.getChildren().addAll(simbol);*/
+			testa1.setFill(Color.WHITE);
+			testa2.setFill(Color.WHITE);
+			testa3.setFill(Color.WHITE);
+			braccia1.setFill(Color.WHITE);
+			braccia2.setFill(Color.WHITE);
+			braccia3.setFill(Color.WHITE);
+			root.getChildren().add(esterno);
+		}
+			root.getChildren().addAll(braccia1, braccia2, braccia3, testa1, testa2, testa3);
 		return root;
 	}
 }
